105a3a43b049cd6c0650962f30ac46a34e1dce05
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-20 12:53:18 +0200

    parnesem failus is pries tai

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+function Hero() {
+  return (
+    <div className="hero">
+      <h3>Hello</h3>
+      {/* <img src="https://picsum.photos/id/8/1200/800" alt="hero" /> */}
+      <img src="/img/hero.jpg" alt="hero" />
+    </div>
+  );
+}
+
+export default Hero;
